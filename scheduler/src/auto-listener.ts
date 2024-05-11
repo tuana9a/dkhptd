0b150589c451a361e373dfa35cebde9dba04a460
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-05-11 12:35:06 +0700

    More one line log and simplify the worker logic #18 (#21)

--- scheduler/src/auto-listener.ts
+++ scheduler/src/auto-listener.ts
@@ -1,23 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import fs from "fs";
 import path from "path";
+import { toJson } from "./utils";
+import logger from "./loggers/logger";
 
 export const setup = (dir: string) => {
   const filepaths = fs.readdirSync(dir).filter(x => x.endsWith(".js"));
-  const loaded = [];
-
+  const loadedPaths = [];
   for (const filepath of filepaths) {
     const relativeFilepath = `${dir}/${filepath}`;
-    const stat = fs.statSync(relativeFilepath);
-
-    if (stat.isDirectory()) {
-      const childLoaded = setup(relativeFilepath);
-      loaded.push(...childLoaded);
-    } else {
-      require(path.resolve(relativeFilepath)).setup();
-      loaded.push(relativeFilepath);
-    }
+    require(path.resolve(relativeFilepath)).setup();
+    loadedPaths.push(relativeFilepath);
   }
-
-  return loaded;
+  logger.info(`Loaded listeners: ${toJson(loadedPaths)}`);
+  return loadedPaths;
 };
