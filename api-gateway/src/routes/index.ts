81b634ef6ce2b2c16fe22863a1b24bf7f4e257d5
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-08-04 21:27:41 +0700

    refactor: docker-compose (#22)

--- api-gateway/src/routes/index.ts
+++ api-gateway/src/routes/index.ts
@@ -5,5 +5,4 @@ import { ExceptionWrapper } from "src/middlewares";
 
 export const router = express.Router();
 
-router.get("/", ExceptionWrapper(async (req, resp) => resp.sendFile("./version.txt", { root: "." })));
-router.get("/version", ExceptionWrapper(async (req, resp) => resp.sendFile("./version.txt", { root: "." })));
+router.get("/", ExceptionWrapper(async (req, resp) => resp.send("server is online")));
